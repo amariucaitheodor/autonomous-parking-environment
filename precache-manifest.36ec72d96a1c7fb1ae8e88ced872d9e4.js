490e6933000feb90291fb21578a187b2c04a6ba9
theodor1289 <amariucaitheodor@gmail.com>
2020-02-08 15:03:24 +0000

    Updates

---
+++ precache-manifest.36ec72d96a1c7fb1ae8e88ced872d9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a332f9fb5c7b66fe31d174bd4063b7",
+    "url": "/operations-monitor/index.html"
+  },
+  {
+    "revision": "332a541c56378775f660",
+    "url": "/operations-monitor/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "51a3cbaaba192b4a79e1",
+    "url": "/operations-monitor/static/css/main.a11950a8.chunk.css"
+  },
+  {
+    "revision": "332a541c56378775f660",
+    "url": "/operations-monitor/static/js/2.132aa017.chunk.js"
+  },
+  {
+    "revision": "d0d3c0d799f73b12414af7b8d54cb9f5",
+    "url": "/operations-monitor/static/js/2.132aa017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a3cbaaba192b4a79e1",
+    "url": "/operations-monitor/static/js/main.528335fa.chunk.js"
+  },
+  {
+    "revision": "fd07156b5d8d3e5d55b7",
+    "url": "/operations-monitor/static/js/runtime-main.ea1b8e5b.js"
+  }
+]);
